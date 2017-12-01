3a598833cb8251058d9489bb4d91283fcb4ec4a2
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-12-01 14:00:00 -0800

    create article like and dislike routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+
   resources :articles do
+    member do
+      put "like", to: "articles#upvote"
+      put "dislike", to: "articles#downvote"
+    end
+    resources :comments
+  end
+
+  resources :users do
     resources :comments
   end
 
   root :to => 'home#index'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :comments
-  end
+
 end
