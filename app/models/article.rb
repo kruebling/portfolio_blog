e292090569a054010a7faa6be0f07df7a048231e
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-12-01 11:27:48 -0800

    add acts as votable migration, create article model which can be voted on by users to be saved to their profile

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ApplicationRecord
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  acts_as_votable
+  belongs_to :user
+
+  validates :user_id, :presence => true, numericality: { only_integer: true }
+  validates :title, :presence => true
+  validates :body, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+  has_many :articles
+
   validates :email, :presence => true
   validates :email, :uniqueness => true
 
