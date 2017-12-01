93d6b84a12422b5221abcebd41e396b13eab7168
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-12-01 11:16:08 -0800

    add photo to articles migration, may modify upload to permit pdfs or add seperate col for different files

---
+++ db/migrate/20171201191306_add_photo_to_article.rb
@@ -0,0 +1,12 @@
+class AddPhotoToArticle < ActiveRecord::Migration[5.1]
+  def self.up
+  change_table :articles do |t|
+      t.attachment :image
+    end
+  end
+
+  def self.down
+    remove_attachment :articles, :image
+  end
+
+end
