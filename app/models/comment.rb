2ebedb70a52ca216f8699361e58749ecfdabb09a
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-12-01 13:35:40 -0800

    add specs for comment and simplecov integration

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  has_one :user
+  has_one :user, through: :article
 
   validates :user_id, :presence => true, numericality: { only_integer: true }
   validates :article_id, :presence => true, numericality: { only_integer: true }
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Comment, 'validation' do
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :article_id }
+  it { should validate_presence_of :body }
+end
+
+describe Comment, 'association' do
+  it { should have_one(:user).through(:article) }
+  it { should belong_to :article }
+end
+
+describe Comment, 'column_specification' do
+  it { should have_db_column(:user_id).of_type(:integer) }
+  it { should have_db_column(:article_id).of_type(:integer) }
+  it { should have_db_column(:body).of_type(:text) }
 end
